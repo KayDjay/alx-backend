ee67d57207d8e3dacbc1832cec4ca29346c4f8ca
kaydjay <khadijatrasaq@gmail.com>
2024-04-03 23:29:57 +0100

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,26 +12,26 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key: str, item: str) -> None:
-            """ Add an item to the cache
-            
-            Args:
-                key (str): The key to associate with the item.
-                item: The item to be added to the cache.
-            """
-            if key is not None and item is not None:
-                self.cache_data[key] = item
+        """ Add an item to the cache
+
+        Args:
+            key (str): The key to associate with the item.
+            item: The item to be added to the cache.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key: str) -> dict:
-            """ Retrieve an item from the cache.
+        """ Retrieve an item from the cache.
 
-            Args:
-                key (str): The key of the item to retrieve from the cache.
+        Args:
+            key (str): The key of the item to retrieve from the cache.
 
-            Returns:
-                The value associated with the given key,
-                or None if the key is not found in the cache.
-            """
-            if key is not None:
-                return self.cache_data.get(key)
-            else:
-                return None
+        Returns:
+            The value associated with the given key,
+            or None if the key is not found in the cache.
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        else:
+            return None
