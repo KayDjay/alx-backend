eb12ae28987e84700f4f7c5f7b3836a453a64ce6
kaydjay <khadijatrasaq@gmail.com>
2024-04-03 22:50:50 +0100

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" BasicCache module """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class """
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
